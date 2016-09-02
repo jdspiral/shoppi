477f05a2443b8eb8fe0d92512685d1ed48bc651e
jdspiral <jdspiral@gmail.com>
2016-09-02 18:48:49 -0400

    Add views for web

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+
+@section('content')
+    <p>Visit the products endpoint <a href="/api/v1/products">/api/v1/products</a></p>
+@endsection
