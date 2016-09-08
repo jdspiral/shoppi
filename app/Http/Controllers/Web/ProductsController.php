34784daa504633b8d6827522828a08a42c4e8ff6
jdspiral <jdspiral@gmail.com>
2016-09-08 16:04:36 -0400

    Add Web folder for web views

---
+++ app/Http/Controllers/Web/ProductsController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\web;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use App\Product;
+use App\Http\Controllers\Controller;
+
+/**
+ * Class ProductsController
+ * @package App\Http\Controllers
+ */
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $products)
+    {
+        $products = $products->all();
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->description = $request->description;
+        $product->save();
+
+        return view('products.view');
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product)
+        {
+            return Response::json([
+               'error' => [
+                   'message' => 'Product does not exist',
+                   'code' => 'add error code here - josh'
+               ]
+            ], 404);
+        }
+
+        return Response::json([
+           'data' => $product->toArray()
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
