2be7d3c1633c8d8c21fbb0dc7dc5736d9d68eeb2
jdspiral <jdspiral@gmail.com>
2016-09-08 18:07:49 -0400

    Add base API controller

---
+++ app/Http/Controllers/API/ApiController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Response;
+
+class ApiController extends Controller
+{
+    /**
+     * @var int
+     */
+    protected $statusCode = 200;
+
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondNotFound($message = 'Not Found')
+    {
+        return $this->setStatusCode(404)->respondWithError($message);
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondInternalError($message = 'Internal Error!')
+    {
+        return $this->setStatusCode(500)->respondWithError($message);
+    }
+
+    /**
+     * @param $message
+     * @return mixed
+     */
+    public function respondCreated($message = 'Entity successfully created!')
+    {
+        return $this->setStatusCode(201)->respond([
+            'message' => $message
+        ]);
+    }
+
+    public function respondUnprocessableEntity($message = 'Unable to process request.')
+    {
+        return $this->setStatusCode(422)
+            ->respondWithError($message);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @return mixed
+     */
+    public function respond($data, $headers = [])
+    {
+        return Response::json($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * @param $message
+     * @return mixed
+     */
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
+}
