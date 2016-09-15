40d9792ae96f85c777306ea64ab6abce2fe8b3a3
jdspiral <jdspiral@gmail.com>
2016-09-15 12:05:53 -0400

    Add product form

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,4 +4,17 @@
 
 @section('content')
     <p>Visit the products endpoint <a href="/api/v1/products">/api/v1/products</a></p>
+    {!! Form::open(['action' => 'Web\ProductsController@store']) !!}
+    {!! Form::label('name', 'Title') !!}
+    {!! Form::text('name') !!}
+    {!! Form::label('description', 'Description') !!}
+    {!! Form::text('description') !!}
+    {!! Form::label('price', 'Price') !!}
+    {!! Form::number('price') !!}
+    {!! Form::submit('Click Me!') !!}
+    {!! Form::close() !!}
+    <hr/>
+    @foreach ($products as $product)
+        <p>This is product is {{ $product->name }}</p>
+    @endforeach
 @endsection
