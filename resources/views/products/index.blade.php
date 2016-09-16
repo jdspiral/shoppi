a0a7c6d2c23feb87b4df87aa1443be94a07a8d9e
jdspiral <jdspiral@gmail.com>
2016-09-15 21:39:05 -0400

    Add bootstrap classes to form

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,14 +5,22 @@
 @section('content')
     <p>Visit the products endpoint <a href="/api/v1/products">/api/v1/products</a></p>
     {!! Form::open(['action' => 'Web\ProductsController@store']) !!}
-    {!! Form::label('name', 'Title') !!}
-    {!! Form::text('name') !!}
-    {!! Form::label('description', 'Description') !!}
-    {!! Form::text('description') !!}
-    {!! Form::label('price', 'Price') !!}
-    {!! Form::number('price') !!}
-    {!! Form::submit('Click Me!') !!}
-    {!! Form::close() !!}
+    <div class="form-group">
+        {!! Form::label('name', 'Title', ['class' => 'control-label']) !!}
+        {!! Form::text('name', '', ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('description', 'Description') !!}
+        {!! Form::text('description', '', ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::label('price', 'Price') !!}
+        {!! Form::number('price', '', ['class' => 'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!! Form::submit('Click Me!') !!}
+        {!! Form::close() !!}
+    </div>
     <hr/>
     @foreach ($products as $product)
         <p>This is product is {{ $product->name }}</p>
