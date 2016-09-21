61a77b195990f684a4246e0932fa2f8e5f66bb41
jdspiral <jdspiral@gmail.com>
2016-09-21 09:41:42 -0400

    Add single product test

--- tests/unit/ProductsTest.php
+++ tests/unit/ProductsTest.php
@@ -14,15 +14,38 @@ class ProductsTest extends ApiTester {
         $this->assertResponseOK();
     }
 
+    /** @test */
+    public function it_fetches_a_single_product()
+    {
+        $this->makeProduct();
+
+        $product = $this->getJson('api/v1/products/1')->data;
+
+        $this->assertResponseOk();
+        $this->assertObjectHasAttributes($product, 'title', 'description', 'price');
+
+    }
+
     private function makeProduct($productFields = [])
     {
         $product = array_merge([
-           'name' => $this->fake->word,
+            'name' => $this->fake->word,
             'description' =>$this->fake->sentence,
             'price' => $this->fake->randomDigit,
         ], $productFields);
 
         while($this->times--) Product::create($product);
+    }
+
+    private function assertObjectHasAttributes()
+    {
+        $args = func_get_args();
+        $object = array_shift($args);
+
+        foreach($args as $attribute)
+        {
+            $this->assertObjectHasAttribute($attribute, $object);
+        }
     }
 
 }
